658bbb2c9fd34248423ed486689fd8bc0d596ad6
JanushiShastri <shastrijanushi@gmail.com>
2025-01-14 17:26:53 -0700

    faking namespaces

---
+++ javaScriptFrameworkDecoded/app_namespace_collision.js
@@ -0,0 +1,15 @@
+var greet = 'Hellooo';
+var greet = 'Hola';
+
+console.log(greet);
+
+var english = {};
+var spanish = {};
+var hindi = {
+    greetings: {
+        basic: "namaste"
+    }
+};
+
+console.log(english); //just a object with greet inside
+
